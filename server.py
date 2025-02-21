310fc781a1f32a23eeca92b9a183d5e038c53ab0
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-02-21 17:21:02 +0800

    update common library

--- server.py
+++ server.py
@@ -2,16 +2,18 @@ from importlib.util import spec_from_file_location, module_from_spec
 import sys
 from common.ModelAPI import ModelHandler, Request
 from common.protocol.tcp import TCPServer
-from common.protocol.protocol import BRequest, BResponse
 from common.protocol.byte_buffter import ByteBuffter
 from common.logger_config import setup_logger
 from logging import getLogger
 
+
 class ModelServer(TCPServer):
     def __init__(self, host: str, port: int, model_name: str):
-        spec = spec_from_file_location(f"{model_name}_model", f"{model_name}_model/{model_name}_model.py")
+        spec = spec_from_file_location(f"{model_name}_model", f"{
+                                       model_name}_model/{model_name}_model.py")
         if spec is None or spec.loader is None:
-            raise FileNotFoundError(f"{model_name}_model/{model_name}_model.py not found")
+            raise FileNotFoundError(
+                f"{model_name}_model/{model_name}_model.py not found")
         module = module_from_spec(spec)
         sys.path.append(f"{model_name}_model")
         spec.loader.exec_module(module)
@@ -21,14 +23,15 @@ class ModelServer(TCPServer):
 
         setup_logger()
 
-        self.logger.info(f"Model Server {model_name} is running on {host}:{port}")
+        self.logger.info(
+            f"Model Server {model_name} is running on {host}:{port}")
 
         super().__init__(self.logger, host, port, self.callback)
 
     def callback(self, data: bytes) -> bytes:
         request_buf = ByteBuffter(data)
         try:
-            request: Request = BRequest.decode(request_buf).get_data()
+            request: Request = Request.decode(request_buf).get_data()
         except:
             self.logger.error(f"Failed to decode request: {data}")
             raise ValueError("Failed to decode request")
@@ -40,9 +43,10 @@ class ModelServer(TCPServer):
             raise ValueError("Failed to invoke model")
         response_buf = ByteBuffter()
 
-        BResponse.encode(response_buf, response)
+        response.encode(response_buf)
 
         return response_buf.to_bytes()
+
 
 if __name__ == "__main__":
     model_name = sys.argv[1]
